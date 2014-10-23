c6ab2c976b4e081deeefdcae50bca13f02e32dcf
VonC <vonc@laposte.net>
2014-10-23 10:02:21 +0200

    Test buffers for a new instance of Pdbg

--- gogdb_test.go
+++ gogdb_test.go
@@ -18,5 +18,15 @@ func TestProject(t *testing.T) {
 			So(Out(), ShouldNotEqual, os.Stdout)
 			So(Err(), ShouldNotEqual, os.Stderr)
 		})
+		Convey("By Default, a new pdbg instance buffer equals to std", func() {
+			apdbg := NewPdbg()
+			So(apdbg.Out(), ShouldEqual, os.Stdout)
+			So(apdbg.Err(), ShouldEqual, os.Stderr)
+		})
+		Convey("By Default, a new pdbg instance set to buffer writes no longer equals to std", func() {
+			apdbg := NewPdbg(SetBuffers)
+			So(apdbg.Out(), ShouldNotEqual, os.Stdout)
+			So(apdbg.Err(), ShouldNotEqual, os.Stderr)
+		})
 	})
 }
