bfcfcd16b3cf9734e8b756141c85b1875156dee9
VonC <vonc@laposte.net>
2014-12-11 10:35:07 +0000

    Fix exclude test

--- godbg_test.go
+++ godbg_test.go
@@ -168,8 +168,6 @@ err2 cerr2
 			So(ErrString(), ShouldEqual,
 				`  [globalPdbgExcludeTest:16] (func.019:167)
     calling no
-    [globalPdbgExcludeTest:17] (func.019:167)
-      gcalled1
       [globalCNo:26] (globalPdbgExcludeTest:17) (func.019:167)
         gcalled2
 `)
